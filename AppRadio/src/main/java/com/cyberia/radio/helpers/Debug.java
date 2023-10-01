8a975f22e4aaff5f861e8216dd619561c5db7868
AlexIger <cyberia@protonmail.com>
2023-10-01 09:37:58 +0700

    Version 6.9 commit changes

--- AppRadio/src/main/java/com/cyberia/radio/helpers/Debug.java
+++ AppRadio/src/main/java/com/cyberia/radio/helpers/Debug.java
@@ -2,7 +2,7 @@ package com.cyberia.radio.helpers;
 
 public final class Debug
 {
-    public static final boolean ENABLED = false;
+    public static final boolean ENABLED = true;
     public static final boolean INFO_ON = ENABLED;
     public static final boolean EXCEPTION_ON = ENABLED;
 }
